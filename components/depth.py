50990351b171d48adb1fa235e4a255f32b9c415e
Jessica <jessicaanhdao@outlook.com>
2019-02-14 14:08:12 -0500

    no message

--- components/depth.py
+++ components/depth.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ctre
 
 class Depth:
@@ -30,37 +29,4 @@ class Depth:
     def execute(self):
         '''This gets called at the end of the control loop'''
         self.depth_motor.set(ctre.WPI_TalonSRX.ControlMode.PercentOutput,self.power)
-=======
-import ctre
-
-class Depth:
-    depth_motor: ctre.WPI_TalonSRX
-
-    # speed is tunable via NetworkTables
-  #  shoot_speed = tunable(1.0)
-
-    def __init__(self):
-        self.enabled = False
-        self.power = 1
-
-    def enable(self):
-        '''Causes the shooter motor to spin'''
-        self.enabled = True
-
-    def is_ready(self):
-        # in a real robot, you'd be using an encoder to determine if the
-        # shooter were at the right speed..
-        return True
-
-    def extend_down(self):
-        return True
-    
-    def unextend_depth(self):
-        self.power = -1
-        return True
-
-    def execute(self):
-        '''This gets called at the end of the control loop'''
-        self.depth_motor.set(ctre.WPI_TalonSRX.ControlMode.PercentOutput,self.power)
->>>>>>> remotes/origin/master
         self.enabled = False
--- components/scoop.py
+++ components/scoop.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ctre
 
 class Scoop:
@@ -25,32 +24,4 @@ class Scoop:
     def execute(self):
         '''This gets called at the end of the control loop'''
         self.scoop_motor.set(ctre.WPI_TalonSRX.ControlMode.PercentOutput,1)
-=======
-import ctre
-
-class Scoop:
-    scoop_motor: ctre.WPI_TalonSRX
-
-    # speed is tunable via NetworkTables
-  #  shoot_speed = tunable(1.0)
-
-    def __init__(self):
-        self.enabled = False
-
-    def enable(self):
-        '''Causes the shooter motor to spin'''
-        self.enabled = True
-
-    def is_ready(self):
-        # in a real robot, you'd be using an encoder to determine if the
-        # shooter were at the right speed..
-        return True
-
-    def scoop_down(self):
-        return True
-
-    def execute(self):
-        '''This gets called at the end of the control loop'''
-        self.scoop_motor.set(ctre.WPI_TalonSRX.ControlMode.PercentOutput,1)
->>>>>>> remotes/origin/master
         self.enabled = False
